3ec63f2d6f4419517fc8fd7b45939c17b10fe8b1
rnwang04 <ruonan1.wang@intel.com>
2026-01-04 10:24:21 +0800

    update

--- videochat_genai/main.cpp
+++ videochat_genai/main.cpp
@@ -38,10 +38,12 @@ void test_video(const CTestParam &param, ov::genai::VLMPipeline &pipe, ov::Tenso
     }
 }
 
+
 #include <openvino/genai/continuous_batching_pipeline.hpp>
-int test_cb_add_request_vs_vlm(const CTestParam &param, ov::Tensor &video)
+int test_cb_add_request_vs_vlm_benchmark(const CTestParam &param, ov::Tensor &video)
 {
-    
+    using Clock = std::chrono::high_resolution_clock;
+
     std::cout << "== Start to load model: " << param.model_path << std::endl;
     ov::AnyMap cfg;
     cfg["ATTENTION_BACKEND"] = "SDPA";
@@ -51,45 +53,99 @@ int test_cb_add_request_vs_vlm(const CTestParam &param, ov::Tensor &video)
 
     ov::genai::GenerationConfig generation_config;
     generation_config.max_new_tokens = 100;
+    generation_config.repetition_penalty = 1.0f; // Default to 1.0f, change to 1.0f program will exit
+
+    const int num_iters = 2;
     std::string sys_prompt;
 	read_txt_to_string("./prompt.txt", sys_prompt);
+    auto start_time = Clock::now();
+    // std::vector<std::string> res_vlm_vec;
+    // std::cout << "==== Test vlm pipe with system prompt." << std::endl;
+    // for (int i = 0; i < num_iters; i++) {
+    //     ov_pipe.start_chat(sys_prompt);
+    //     auto res_vlm_1 = ov_pipe.generate(prompt,
+    //                                       ov::genai::images(std::vector<ov::Tensor>{video}),
+    //                                       ov::genai::generation_config(generation_config));
+    //     res_vlm_vec.push_back(res_vlm_1.texts[0]);
+    //     ov_pipe.finish_chat();
+    //     std::cout << "    == get_prepare_embeddings_duration = " << res_vlm_1.perf_metrics.get_prepare_embeddings_duration().mean << std::endl;
+    //     std::cout << "    == TTFT = " << res_vlm_1.perf_metrics.get_ttft().mean << " +- " << res_vlm_1.perf_metrics.get_ttft().std << std::endl;
+    //     std::cout << "    == TPOT = " << res_vlm_1.perf_metrics.get_tpot().mean << " +- " << res_vlm_1.perf_metrics.get_tpot().std << std::endl;
+    // }
 
+    // auto end_time = Clock::now();
+    // auto duration = end_time - start_time;
+    // auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(duration);
+    // std::cout << "E2E VLM pipeline time : " << ms.count() << "ms\n";
+
+    // ov_pipe.finish_chat();
     std::vector<std::string> res_vlm_vec;
-    for (int i = 0; i < 10; i++) {
-        ov_pipe.start_chat(sys_prompt);
-        auto res_vlm_1 = ov_pipe.generate(prompt,
+    generation_config.apply_chat_template = false;
+    std::cout << "==== Test vlm pipe with image in prompt." << std::endl;
+    auto complete_template = "<|im_start|>system\n" + sys_prompt + "<|im_end|>\n<|im_start|>user\n" + "<|image_1|>\n" + prompt + "<|im_end|>\n<|im_start|>assistant\n";
+    for (int i = 0; i < num_iters; i++) {
+        auto res_vlm_1 = ov_pipe.generate(complete_template,
                                           ov::genai::images(std::vector<ov::Tensor>{video}),
                                           ov::genai::generation_config(generation_config));
         res_vlm_vec.push_back(res_vlm_1.texts[0]);
-        ov_pipe.finish_chat();
+        std::cout << "    == get_prepare_embeddings_duration = " << res_vlm_1.perf_metrics.get_prepare_embeddings_duration().mean << std::endl;
+        std::cout << "    == TTFT = " << res_vlm_1.perf_metrics.get_ttft().mean << " +- " << res_vlm_1.perf_metrics.get_ttft().std << std::endl;
+        std::cout << "    == TPOT = " << res_vlm_1.perf_metrics.get_tpot().mean << " +- " << res_vlm_1.perf_metrics.get_tpot().std << std::endl;
     }
 
     auto scheduler_config = ov::genai::SchedulerConfig();
+    // scheduler_config.enable_prefix_caching = false;
+    // scheduler_config.dynamic_split_fuse = true;
+    // scheduler_config.max_num_batched_tokens = 2400;
     auto cb_pipe = ov::genai::ContinuousBatchingPipeline(
         param.model_path,
         scheduler_config,
         param.device);
     auto tokenizer = cb_pipe.get_tokenizer();
 
+    std::cout << "==== Test cb pipe with image and system prompt." << std::endl;
+    start_time = Clock::now();
     std::vector<std::string> res_cb_vec;
-    for (int i = 0; i < 5; i++) {
-        cb_pipe.start_chat(sys_prompt);
-        auto handle = cb_pipe.add_request(i, prompt, std::vector<ov::Tensor>{video},
+    std::vector<ov::genai::GenerationHandle> handles;
+    generation_config.apply_chat_template = false;
+    for (int i = 0; i < num_iters; i++) {
+        auto handle = cb_pipe.add_request(i, complete_template, std::vector<ov::Tensor>{video},
                                           generation_config);
-        while (handle->get_status() != ov::genai::GenerationStatus::FINISHED)
-            cb_pipe.step();
-            cb_pipe.finish_chat();
-    
-        auto outputs = handle->read_all();
-    
+        handles.push_back(handle);
+    }
+
+    while (cb_pipe.has_non_finished_requests()) {
+        cb_pipe.step();
+    }
+
+    for (int i = 0; i < num_iters; i++) {
+        auto outputs = handles[i]->read_all();
         auto res_cb_pipeline = tokenizer.decode(outputs[0].generated_ids);
         res_cb_vec.push_back(res_cb_pipeline);
     }
 
-    for (int i = 0; i < 5; i++) {
+    auto end_time = Clock::now();
+    auto duration = end_time - start_time;
+    auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(duration);
+    std::cout << "E2E CB pipeline time : " << ms.count() << "ms\n";
+
+    // for (int i = 0; i < num_iters; i++) {
+    //     auto cmp_rslt = res_vlm_vec2[i].compare(res_vlm_vec[i].c_str());
+    //     std::cout << "cmp_result [" << i << "] : " << cmp_rslt << std::endl;
+    //     if (cmp_rslt != 0)
+    //     {
+    //         std::cout << "    =============================" << std::endl;
+    //         std::cout << "    rslt_vlm = " << res_vlm_vec[i] << std::endl;
+    //         std::cout << "    *****************************" << std::endl;
+    //         std::cout << "    rslt_vlm2  = " << res_vlm_vec2[i] << std::endl;
+    //         std::cout << "    =============================" << std::endl;
+    //     }
+    // }
+
+    for (int i = 0; i < num_iters; i++) {
         auto cmp_rslt = res_cb_vec[i].compare(res_vlm_vec[i].c_str());
         std::cout << "cmp_result [" << i << "] : " << cmp_rslt << std::endl;
-        if (cmp_rslt != 0)
+        // if (cmp_rslt != 0)
         {
             std::cout << "    =============================" << std::endl;
             std::cout << "    rslt_vlm = " << res_vlm_vec[i] << std::endl;
@@ -97,6 +153,71 @@ int test_cb_add_request_vs_vlm(const CTestParam &param, ov::Tensor &video)
             std::cout << "    rslt_cb  = " << res_cb_vec[i] << std::endl;
             std::cout << "    =============================" << std::endl;
         }
+    }
+
+    return EXIT_SUCCESS;
+}
+
+#include <openvino/genai/continuous_batching_pipeline.hpp>
+int test_cb_add_request(const CTestParam &param, ov::Tensor &video)
+{
+    using Clock = std::chrono::high_resolution_clock;
+
+    ov::genai::GenerationConfig generation_config;
+    generation_config.max_new_tokens = 100;
+    generation_config.repetition_penalty = 1.0f; // Default to 1.0f, change to 1.0f program will exit
+
+    const int num_iters = 2;
+    std::string sys_prompt;
+	read_txt_to_string("./prompt.txt", sys_prompt);
+    // read_txt_to_string("./short_prompt.txt", sys_prompt);
+    auto start_time = Clock::now();
+
+    auto scheduler_config = ov::genai::SchedulerConfig();
+    scheduler_config.enable_prefix_caching = false;
+    scheduler_config.dynamic_split_fuse = true;
+    // scheduler_config.max_num_batched_tokens = 4000;
+    auto cb_pipe = ov::genai::ContinuousBatchingPipeline(
+        param.model_path,
+        scheduler_config,
+        param.device);
+    auto tokenizer = cb_pipe.get_tokenizer();
+
+    std::cout << "==== Test cb pipe with image and system prompt." << std::endl;
+    std::string prompt = param.prompt;
+    auto complete_template = "<|im_start|>system\n" + sys_prompt + "<|im_end|>\n<|im_start|>user\n" + "<|image_1|>\n" + prompt + "<|im_end|>\n<|im_start|>assistant\n";
+    start_time = Clock::now();
+    std::vector<std::string> res_cb_vec;
+    std::vector<ov::genai::GenerationHandle> handles;
+    generation_config.apply_chat_template = false;
+    for (int i = 0; i < num_iters; i++) {
+        std::cout << "==== Test add request for request " << i << std::endl;
+        auto handle = cb_pipe.add_request(i, complete_template, std::vector<ov::Tensor>{video},
+                                          generation_config);
+        handles.push_back(handle);
+    }
+    std::cout << "==== Finish add request." << std::endl;
+    while (cb_pipe.has_non_finished_requests()) {
+        cb_pipe.step();
+    }
+    std::cout << "==== Finish step()." << std::endl;
+
+    for (int i = 0; i < num_iters; i++) {
+        auto outputs = handles[i]->read_all();
+        auto res_cb_pipeline = tokenizer.decode(outputs[0].generated_ids);
+        res_cb_vec.push_back(res_cb_pipeline);
+    }
+
+    auto end_time = Clock::now();
+    auto duration = end_time - start_time;
+    auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(duration);
+    std::cout << "E2E CB pipeline time : " << ms.count() << "ms\n";
+
+    for (int i = 0; i < num_iters; i++) {
+        std::cout << "cb_result [" << i << "] : " << std::endl;
+        std::cout << "    =============================" << std::endl;
+        std::cout << "    rslt_cb  = " << res_cb_vec[i] << std::endl;
+        std::cout << "    =============================" << std::endl;
     }
 
     return EXIT_SUCCESS;
@@ -135,13 +256,13 @@ int main(int argc, char *argv[])
                                           tensor_shape,
                                           ov::element::f32);
         } else {
-            ov::Shape tensor_shape{12, 3, 224, 224};
+            ov::Shape tensor_shape{4, 3, 224, 224};
             video = ov::Tensor(ov::element::f32, tensor_shape); // video features which have been preprocessed
         }
-        test_video(param, pipe, video);
-        test_cb_add_request_vs_vlm(param, video);
+        test_cb_add_request_vs_vlm_benchmark(param, video);
+        // test_cb_add_request(param, video);
     }
-    
+
     catch (const std::exception &error)
     {
         std::cerr << "Catch exceptions: " << error.what() << '\n';
